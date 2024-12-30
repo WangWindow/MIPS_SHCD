35ab891c45fd39940db1b54013a6bf3c659eb694
WangWindow <1598593280@qq.com>
2024-12-30 22:26:46 +0800

    Update: operate mul&div.

--- MyCPU/MyCPU.gen/sources_1/ip/MyRom/MyRom_stub.vhdl
+++ MyCPU/MyCPU.gen/sources_1/ip/MyRom/MyRom_stub.vhdl
@@ -2,7 +2,7 @@
 -- Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
--- Date        : Mon Dec 30 18:09:24 2024
+-- Date        : Mon Dec 30 21:47:31 2024
 -- Host        : XIAOXIN running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode synth_stub
 --               c:/Users/weidong/Desktop/MIPS_SHCD/MyCPU/MyCPU.gen/sources_1/ip/MyRom/MyRom_stub.vhdl
